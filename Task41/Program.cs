a289f56d88809b4a38dcf10125e5e6eb1bb305e6
Aygun <aygun.gasimova@inbox.ru>
2022-12-11 19:35:42 +0400

    Task41 completed

---
+++ Task41/Program.cs
@@ -0,0 +1,21 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void QofPos(int[] arr, out int N)   // QofPos is Quantity of POsitive Numbers
+{
+    N = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            N ++;
+        }
+    }
+    
+}
+
+Console.WriteLine("Insert numbers");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+QofPos(arr , out int N );
+Console.WriteLine($"Quantity of positive numbers is "+ N);
+
+
